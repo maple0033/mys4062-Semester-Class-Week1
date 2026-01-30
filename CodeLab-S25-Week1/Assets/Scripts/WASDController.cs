a430fa7f011633cbdd1d10d37d148fd9304549f8
maple0033 <so.marielyulo@gmail.com>
2026-01-30 07:58:30 -0500

    Created a very buggy bowling game
    implemented a score tracker, collision, and then added the 3d game objects. also set up a transform so that the ball returns to start.
    Mostly added all these to WASD script

--- CodeLab-S25-Week1/Assets/Scripts/WASDController.cs
+++ CodeLab-S25-Week1/Assets/Scripts/WASDController.cs
@@ -1,19 +1,28 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
     
 public class WASDController : MonoBehaviour
 { 
-    Rigidbody rb;
+    Rigidbody rb; //rigidbody of the gameobject that this script is attached to
 
-    public float moveForce = 10f;
+    public float moveForce = 10f; //the force we add to the gameobject this script is attached to to make it move 
 
-    public Key keyUp = Key.W;
+    public Key keyUp = Key.W; //what key you must press to make things go up the y axis
+    public Key keyDown = Key.S; //what key you must press to make things go down the y axis
+    public Key keyLeft = Key.A;
+    public Key keyRight = Key.D;
 
-    Keyboard keyboard = Keyboard.current;
+    Keyboard keyboard = Keyboard.current; //what keyboard you will use to take in input 
+
+    public float score = 0;
+    public GameObject KillForceCheck;
+    private bool resetGame = false;
     
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>(); //getcomponent looks for a component of this type on the gameobject this script is attached to
+        Debug.Log(score);
     }
 
     void FixedUpdate()
@@ -21,13 +30,61 @@ public class WASDController : MonoBehaviour
 
         if (keyboard[keyUp].isPressed)
         {
-            rb.AddForce(Vector3.up * moveForce, ForceMode.Acceleration);
+            rb.AddForce(Vector3.forward * moveForce, ForceMode.Acceleration); // give the object an upward force 
         }
-        
+
+        if (keyboard[keyDown].isPressed)
+        {
+            rb.AddForce(Vector3.down * moveForce, ForceMode.Acceleration);
+        }
+
+        if (keyboard[keyLeft].isPressed)
+        {
+            rb.AddForce(Vector3.left * moveForce, ForceMode.Acceleration);
+        }
+
+        if (keyboard[keyRight].isPressed)
+        {
+            rb.AddForce(Vector3.right * moveForce, ForceMode.Acceleration);
+        }
         
         // if(Input.GetKey(KeyCode.S))
         // {
-        //     rb.AddForce(Vector3.down * moveForce, ForceMode.Acceleration);
+        //     rb.AddForce(Vector3.down * moveForce, ForceMode.Acceleration); // give the object a downard force 
         // }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Pin"))
+        {
+            score += 1;
+            Debug.Log(score);
+        }
+    }
+
+    private void OnTriggerEnter (Collider other)
+    { 
+        if (other.gameObject == KillForceCheck)
+        {
+            Debug.Log("GAME OVER");
+            moveForce = 0f;
+            resetGame = true;
+            TeleportBall();
+        }
+        
+    }
+
+    private void TeleportBall()
+    {
+        if (resetGame == true)
+        {
+            Debug.Log("Your Final Score: " + score);
+            score = 0;
+            resetGame = false;
+            rb.position = new Vector3(0, 0, -6); //
+            moveForce = 10f;
+
+        }
     }
 }
